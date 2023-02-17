f1d4767c5b78df2cc8e01cfc9a2778c45dc40bf8
ultra1008 <ultra921008@gmail.com>
2023-02-17 14:28:53 -0500

    T-11 | update docker compose to run to localhost

--- start.sh
+++ start.sh
@@ -5,10 +5,5 @@ docker-compose -f docker-compose.yml down
 #Maven build
 mvn clean package -Dmaven.test.skip
 
-#Docker build
-docker build ./landscape/discovery-service -t hayat_discovery_service:latest
-docker build ./landscape/config-service -t hayat_config_service:latest
-docker build ./landscape/gateway -t hayat_gateway_service:latest
-
 #Docker up
-docker-compose -f docker-compose.yml up -d
+docker-compose -f ./docker-compose.yml --env-file ./.env up --build -d
