3cbbfac5e38d6e4a2ae61803e7bdadf7036540e8
ultra1008 <ultra921008@gmail.com>
2023-02-18 04:59:32 -0500

    T-11 | Running authorization service

--- landscape/authorization/src/main/java/com/harera/hayat/authorization/security/SecurityConfig.java
+++ landscape/authorization/src/main/java/com/harera/hayat/authorization/security/SecurityConfig.java
@@ -1,59 +1,31 @@
 package com.harera.hayat.authorization.security;
 
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter  {
+public class SecurityConfig {
 
-    private static final String[] OPEN_APIS = {"/api/v1/auth/**", "/api/v1/oauth/**"};
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.csrf().disable().cors().and().formLogin().disable()
-                .authorizeHttpRequests().anyRequest().authenticated().requestMatchers(OPEN_APIS).permitAll();
-    }
-
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider provider = keycloakAuthenticationProvider();
-        provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(provider);
-    }
+    private static final String[] OPEN_APIS = {
+            "/api/v1/auth/**", "/api/v1/oauth/**",
+            "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**", "/swagger-resources/**"};
 
     @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().formLogin().disable()
+                .authorizeHttpRequests().requestMatchers(OPEN_APIS).permitAll()
+                .anyRequest().authenticated();
+        return http.build();
     }
 
     @Bean
     public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void init(WebSecurity builder) throws Exception {
-
-    }
-
-    @Override
-    public void configure(WebSecurity builder) throws Exception {
-
     }
 }
