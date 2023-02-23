aab87dd0ed1a5026ec2f4b3b6a8795c0978e49fb
ultra1008 <ultra921008@gmail.com>
2023-02-23 00:46:34 -0500

    T-19 | Create common service

---
+++ services/common/src/main/java/com/harera/hayat/common/controller/city/StateController.java
@@ -0,0 +1,43 @@
+package com.harera.hayat.common.controller.city;
+
+
+import com.harera.hayat.framework.model.city.StateResponse;
+import com.harera.hayat.framework.service.StateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/states")
+@Tag(name = "State")
+public class StateController {
+
+    @Autowired
+    private StateService stateService;
+
+    @GetMapping
+    @Operation(summary = "List", description = "list all states", tags = "State",
+                    responses = { @ApiResponse(responseCode = "200",
+                                    description = "success|Ok") })
+    public ResponseEntity<List<StateResponse>> list() {
+        List<StateResponse> list = stateService.list();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get", description = "Get state data", tags = "State",
+                    responses = { @ApiResponse(responseCode = "200",
+                                    description = "success|Ok") })
+    public ResponseEntity<StateResponse> get(@PathVariable("id") int id) {
+        StateResponse stateResponse = stateService.get(id);
+        return ResponseEntity.ok(stateResponse);
+    }
+}
---
+++ services/common/src/main/java/com/harera/hayat/common/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.harera.hayat.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] OPEN_APIS = {
+            "/api/v1/auth/**", "/api/v1/oauth/**", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**", "/swagger-resources/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests()
+                .requestMatchers(OPEN_APIS).permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt).build();
+    }
+}
