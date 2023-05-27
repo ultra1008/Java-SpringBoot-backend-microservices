c51bd5bc8e249716aac656e470cefeca623df82f
ultra1008 <ultra921008@gmail.com>
2023-05-27 01:18:07 -0400

    T-71 || add state to city response

--- components/framework/src/main/java/com/harera/hayat/framework/model/city/CityResponse.java
+++ components/framework/src/main/java/com/harera/hayat/framework/model/city/CityResponse.java
@@ -2,9 +2,13 @@ package com.harera.hayat.framework.model.city;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true, value = { "active" })
 public class CityResponse extends CityDto {
+
+    @JsonProperty("state")
+    private StateResponse state;
 }
