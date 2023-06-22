3c7a804f7a3eff4c2d47bfc623c7d9a2dec83e10
ultra1008 <ultra921008@gmail.com>
2023-06-22 12:05:16 -0400

    T-71 || add global cors

--- landscape/gateway/src/main/java/com/harera/hayat/gateway/CorsConfig.java
+++ landscape/gateway/src/main/java/com/harera/hayat/gateway/CorsConfig.java
@@ -1,8 +1,20 @@
 package com.harera.hayat.gateway;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebFluxConfigurer {
@@ -10,7 +22,28 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Set the allowed HTTP methods
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*").maxAge(3600);
+    }
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = exchange.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.setAccessControlAllowOrigin("*");
+                headers.setAccessControlAllowHeaders(Arrays.asList("*"));
+                headers.setAccessControlAllowMethods(Arrays.asList(HttpMethod.GET,
+                                HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE));
+                headers.setAccessControlMaxAge(Duration.ofSeconds(3600));
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(exchange);
+        };
     }
 }
