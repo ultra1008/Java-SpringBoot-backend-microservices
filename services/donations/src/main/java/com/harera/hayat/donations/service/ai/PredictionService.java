c6806aee45b46394ae0d7f903e4ac6147f6a7983
ultra1008 <ultra921008@gmail.com>
2023-06-27 08:09:08 -0400

    T-72 || add prediction service

--- services/donations/src/main/java/com/harera/hayat/donations/service/ai/PredictionService.java
+++ services/donations/src/main/java/com/harera/hayat/donations/service/ai/PredictionService.java
@@ -1,20 +1,29 @@
 package com.harera.hayat.donations.service.ai;
 
 import com.harera.hayat.donations.model.ai.Prediction;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PredictionService {
 
-    private final RestTemplate restTemplate;
-
-    public PredictionService() {
-        this.restTemplate = new RestTemplate();
-    }
-
     public Prediction predict(String text) {
-        return restTemplate.postForObject("http://146.190.206.136:5000/predict", text,
-                        Prediction.class);
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String url = "http://146.190.206.136:5000/predict";
+
+        String requestBody = "{\"text\":\"%s\"}".formatted(text);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<Prediction> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Prediction.class);
+        return responseEntity.getBody();
     }
 }
