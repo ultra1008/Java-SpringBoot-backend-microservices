8db3800ac3753ac7149dbfedb24a032cd1c30ac6
ultra1008 <ultra921008@gmail.com>
2023-07-06 13:27:29 -0400

    T-73 || alter table user

--- components/framework/src/main/java/com/harera/hayat/framework/model/BaseDocument.java
+++ components/framework/src/main/java/com/harera/hayat/framework/model/BaseDocument.java
@@ -3,6 +3,7 @@ package com.harera.hayat.framework.model;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Setter
@@ -14,4 +15,8 @@ public class BaseDocument {
 
     @Field(name = "active")
     private boolean active = true;
+
+    @Indexed(name = "created_at", expireAfterSeconds = 1296000)
+    @Field(name = "created_at")
+    private long createdAt = System.currentTimeMillis();
 }
